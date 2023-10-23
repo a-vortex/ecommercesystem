6789b872819ef009929d9d99aa4131dc13d8608d
Yuri Wada <yuri.wadah@gmail.com>
2023-10-23 11:33:37 -0300

    starter test
    refs []

--- firstcommit.cpp
+++ firstcommit.cpp
@@ -1,1 +1,14 @@
-//Iniciando os testes do github
+/*
+    Starting tests and practices on GitHub.
+    Author: Yuri Wada
+    yuriwada@ufmg.br
+*/
+
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
