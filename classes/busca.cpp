dd235fc54abec89b2521ea291bfade47fb82e0c2
Kauan Silva <kauansilva@cpejr.com.br>
2023-11-14 12:58:29 -0300

    update

--- classes/busca.cpp
+++ classes/busca.cpp
@@ -14,10 +14,13 @@ void Busca::busca_produtos_tipo(std::string tipo)
         if (std::get<1>(buscador) == tipo)
         {
             std::cout << std::endl;
-            std::cout << "Produto encontrado, nome:" << std::get<0>(buscador) << std::endl;
+            std::cout << "Lista de produtos do tipo:" << std::get<1>(buscador) << std::endl;
+            std::cout << "tipo:" << std::get<1>(buscador) << std::endl;
+            std::cout << "preço:" << std::get<2>(buscador) << std::endl;
+
             produto_encontrado = true;
         }
-        if(!produto_encontrado)
+        if (!produto_encontrado)
         {
             std::cout << "Produto não encontrado." << std::endl;
         }
@@ -34,11 +37,12 @@ void Busca::busca_produtos_nome(std::string nome)
         if (std::get<0>(buscador) == nome)
         {
             std::cout << std::endl;
-            std::cout << "Produto encontrado, tipo:" << std::get<1>(buscador) << std::endl;
-            std::cout << "Preço:" << std::get<2>(buscador) << std::endl;
+            std::cout << "Produto " << std::get<0>(buscador) << " encontrado" << std::endl;
+            std::cout << "tipo:" << std::get<1>(buscador) << std::endl;
+            std::cout << "preço:" << std::get<2>(buscador) << std::endl;
             produto_encontrado = true;
         }
-        if(!produto_encontrado)
+        if (!produto_encontrado)
         {
             std::cout << "Produto não encontrado." << std::endl;
         }
@@ -52,13 +56,16 @@ void Busca::busca_produtos_preco(float preco)
     for (const std::tuple<std::string, std::string, float> &buscador : nome_tipo_preco)
     {
 
-        if (std::get<2>(buscador) == preco)
+        if (std::get<2>(buscador) <= preco)
         {
             std::cout << std::endl;
-            std::cout << "Produto encontrado, nome:" << std::get<0>(buscador) << std::endl;
+            std::cout << "Produto com o preços iguais e menores que:" << std::get<2>(buscador) << std::endl
+                      << "nome:" << std::get<0>(buscador) << std::endl;
+            std::cout << "tipo:" << std::get<1>(buscador) << std::endl;
+
             produto_encontrado = true;
         }
-        if(!produto_encontrado)
+        if (!produto_encontrado)
         {
             std::cout << "Produto não encontrado." << std::endl;
         }
@@ -70,5 +77,7 @@ int main()
     Busca busca;
     Produto p("Produto1", "Testando o produto 1", "2154", "teste", 20.2);
     busca.nome_tipo_preco.push_back(std::make_tuple(p.GetName(), p.GetType(), p.GetPrice()));
-    busca.busca_produtos_tipo("tipofalso");
+    busca.busca_produtos_tipo("teste");
+    busca.busca_produtos_nome("Produto1");
+    busca.busca_produtos_preco(20.2);
 }
