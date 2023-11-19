94baa7efbcef8482cab012405749338935b2f90b
Yuri Wada <yuri.wadah@gmail.com>
2023-11-19 17:54:30 -0300

    Comenta a classe Busca
    +adiciona comentarios na classe Busca

--- classes/busca.hpp
+++ classes/busca.hpp
@@ -15,10 +15,24 @@ private:
     std::vector<float> preco_produto;
 
 public:
-    std::vector<std::tuple<std::string, std::string, float>> nome_tipo_preco;
 
+    /// @brief Faz a busca do produto com o nome especificado
+    ///        em uma determinada lista
+    /// @param nome do produto que quer pesquisar
+    /// @param _lista_de_produtos onde o produto esta inserido
+    /// @return o produto especifiado
     Produto busca_produtos_nome(const std::string nome, const std::vector<Produto> &_lista_de_produtos);
+
+    /// @brief Faz a busca dos produtos com tipo determinado
+    /// @param tipo do produto que quer pesquisar
+    /// @param _lista_de_produtos onde o produto esta inserido
+    /// @return uma lista de produtos com o tipo especificado
     std::vector<Produto> busca_produtos_tipo(const std::string tipo, const std::vector<Produto> &_lista_de_produtos);
+
+    /// @brief Faz a busca dos produtos com preco determinado
+    /// @param preco do produto que quer pesquisar
+    /// @param _lista_de_produtos onde o produto esta inserido
+    /// @return uma lista de produtos com o preco especificado
     std::vector<Produto> busca_produtos_preco(float preco, const std::vector<Produto> &_lista_de_produtos);
 };
 
