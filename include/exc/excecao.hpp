633e93523ad8d1658195230d34a4a779c633b03c
Clara <mccontarinid@gmail.com>
2023-11-29 13:49:17 +0000

    Atualização da branch com a main

--- include/exc/excecao.hpp
+++ include/exc/excecao.hpp
@@ -2,9 +2,7 @@
 
     excecao.hpp
     Author: M. Clara Contarini
-            Júlia Raposo Ferreira
     mccontarinid@gmail.com
-    juliarferreira00@gmail.com
 
     Exceções do sistema
 
@@ -14,32 +12,6 @@
 #define EXCECAO_H
 
 #include <iostream>
-#include <string>
-#include <stdexcept>
-
-class NotFound : public std::exception
-{
-    public:
-    char const *what() const noexcept override;
-};
-
-class AlreadyExists : public std::exception
-{
-    public:
-    char const *what() const noexcept override;
-};
-
-class InvalidInput : public std::exception
-{
-    public:
-    char const *what() const noexcept override;
-};
-
-class NoAvailablePay : public std::exception
-{
-    public:
-    char const *what() const noexcept override;
-};
 
 
 
--- src/exc/excecao.cpp
+++ src/exc/excecao.cpp
@@ -1,21 +1,2 @@
 #include "../../include/exc/excecao.hpp"
-
-char const *NotFound::what() const noexcept 
-{
-    return "Produto não encontrado.";
-}
-
-char const *AlreadyExists::what() const noexcept
-{
-    return "Identificador já existente.";
-}
-
-char const *InvalidInput::what() const noexcept
-{
-    return "Entrada inválida.";
-};
-
-char const *NoAvailablePay::what() const noexcept
-{
-    return "Nenhum método de pagamento cadastrado.";
-}
+''
