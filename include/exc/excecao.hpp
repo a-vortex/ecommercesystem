dad8c5c7818c270044906baf90feb70f6bbb4022
Clara <mccontarinid@gmail.com>
2023-11-29 13:53:35 +0000

    Corrigindo a cagada que eu fiz

--- include/exc/excecao.hpp
+++ include/exc/excecao.hpp
@@ -2,7 +2,9 @@
 
     excecao.hpp
     Author: M. Clara Contarini
+                Júlia Raposo Ferreira
     mccontarinid@gmail.com
+    juliarferreira00@gmail.com
 
     Exceções do sistema
 
@@ -12,7 +14,31 @@
 #define EXCECAO_H
 
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
+class NotFound : public std::exception
+{
+    public:
+    char const *what() const noexcept override;
+};
 
+class AlreadyExists : public std::exception
+{
+    public:
+    char const *what() const noexcept override;
+};
+
+class InvalidInput : public std::exception
+{
+    public:
+    char const *what() const noexcept override;
+};
+
+class NoAvailablePay : public std::exception
+{
+    public:
+    char const *what() const noexcept override;
+};
 
 #endif /* EXCECAO_H */
