f393af134a38f0a486c5b04fd529a951756797bc
Clara <mccontarinid@gmail.com>
2023-11-29 01:36:33 +0000

    Adição parcial do acesso ao carrinho

--- include/ui/acessocarrinho.hpp
+++ include/ui/acessocarrinho.hpp
@@ -7,3 +7,18 @@
 */
 
 #pragma once
+
+#include "menu.hpp"
+#include "../sys/carrinho.hpp"
+
+namespace ecommerce::ui
+{
+    class CartAcess : public Menu
+    {
+        public:
+            CartAcess();
+            /// @brief Permite escolher a tela seguinde na navegação
+            /// @param  option Número da opção escolhida
+            Menu *next(unsigned option) override;
+    };
+}
--- src/ui/acessocarrinho.cpp
+++ src/ui/acessocarrinho.cpp
@@ -1,2 +1,51 @@
 #include "../../include/ui/acessocarrinho.hpp"
 #include <iostream>
+
+namespace ecommerce::ui
+{
+    CartAcess::CartAcess(){
+        _title = "Carrinho De Compras";
+        _options.push_back("1- Adicionar Produto");
+        _options.push_back("2- Remover Produto");
+        _options.push_back("3- Prosseguir com a compra");
+        _options.push_back("4- Voltar ao Menu");
+    }
+    Menu *CartAcess(unsigned option)
+    {
+
+    
+        switch (option)
+        {
+            case 1:
+            {
+                unsigned quantidade;
+                std::string objeto;
+                std::cout<< std::endl << "> Digite o produto e a quantidade que deseja adicionar:";
+
+                /// @todo instanciar/buscar produto em arquivo estoque para passagem de parâmetro
+                std::cin >> objeto >> quantidade;
+
+                // Produto produto();
+                // marquinhos->adiciona_produto(produto, quantidade);
+                // CartAcess proximo;
+                // proximo.render();
+                break;
+            }
+            case 2:
+            {
+                ///@todo
+                break;
+            }
+            case 3:
+            {
+                ///@todo acesso ao pagamento
+                break;
+            }
+            case 4:
+            {
+                ///@todo retorna ao menu cliente
+                break;
+            }
+        }
+    }
+}
